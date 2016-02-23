4a9056f3078b85a2c80bac9ced00f4cc11aee8c5
patagonia28 <zaxarina.anastasya@gmail.com>
2016-02-23 18:22:19 +0300

    Задание 3

---
+++ 3.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[2]:
+
+#создание БД
+data = ('иван петров','георгий парфенов','ирина иванова','николай носков','максим радулов','семен слепаков')
+import pickle
+
+#проверка, заполнен ли файл
+try: 
+    f=open('data.pickle', 'rb')
+    print(pickle.load(f))
+except IOError:
+    f=open('data.pickle', 'wb')
+    pickle.dump(data, f)
+
+
+# In[3]:
+
+def func():
+    q=input('Что ищете? Введите "Имя" или "Фамилия"')
+    i=0
+    for n in data:
+        a=n.split()
+        for b in a:
+            if b==q:
+                print(a)
+                i=i+1
+    if i==0:
+        print('Попробуйте еще раз')
+        func()
+        
+#запускаем функцию фильтрации
+func()
+
